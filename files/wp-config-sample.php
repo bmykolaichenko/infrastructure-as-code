6f4de551577f7c503c16b77ad59984b3f3b67ec9
bmykolaichenko <bmykolaichenko@gmail.com>
2020-11-05 19:42:35 +0200

    new edits

---
+++ files/wp-config-sample.php
@@ -0,0 +1,26 @@
+. . .
+
+define('AUTH_KEY',         '$4H!:Kp_jr|.bzwh}L?PL)=4;mU:`Z<fRl+N%!6a+41|uT9UNDKIHT$L|,6ZBp++');
+define('SECURE_AUTH_KEY',  'NK7#_9FM-Z7k|UTX<+pe0EMmV8<n^Ixa`+B4~:f3[d.+:ln]S^Qp4QydU^WiC}KK');
+define('LOGGED_IN_KEY',    '>,-)PW>`1]1ZrxE/5u!0EgTX;V]$GPDr}mQtcr+J|?CTCLcRZmO`+b}]|N.,qz }');
+define('NONCE_KEY',        '2><8<}/);-qGUc+`]=,u,q$LuDW%3*397(yg!;]v^cdok`n-Zr8|p0XQ7{[Fdqyq');
+define('AUTH_SALT',        'Sv8&4(]A^}V%[j6o:MQWga[ ?+E|8 bp*+1bV*Zl76NZ/$NUJb)+EIp,i29iY=k5');
+define('SECURE_AUTH_SALT', '_E{bq.}8BDsjFRa}rnZ6Noehi2TT<k>z@G-a!,!HINg;&T-.MOI(r`8Yn{PA%{Z>');
+define('LOGGED_IN_SALT',   'Vn]U^gB+na|`>e)&iwjw9tU|OY`M2w.b;iw|p-z|={Y]ofvL,FQ%5PCjk(leY*w-');
+define('NONCE_SALT',       'rn7TS,:.&vytv269pJ-U&W%Z>]+Y8]^EP2w&QeILH.w88:6b!U<3^O`YH;PT*ZpG');
+
+
+. . .
+. . .
+
+define('DB_NAME', 'mysql-server');
+
+/** MySQL database username */
+define('DB_USER', 'root');
+
+/** MySQL database password */
+define('DB_PASSWORD', 'jrP5(pM4P)8;{v(q');
+
+. . .
+
+define('FS_METHOD', 'direct');
